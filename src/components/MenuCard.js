d42a521468c4c9aa49f8e468fd71f368d9e2d979
harmanjot2002 <harmanjot0555.be21@chitkara.edu.in>
2023-07-14 09:15:07 +0530

    Navbar

--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -7,18 +7,20 @@ const MenuCard = ({menuData}) => {
         <>
             <section className="main-card--cointainer">
             {menuData.map((currElem)=>{
+                //Destructing in Arrays
+                const{id,name,category,image,description}=currElem;
                 return(
                     <>
-                    <div className="card-container" key={currElem.id}>
+                    <div className="card-container" key={id}>
                         <div className="card">
                             <div className="card-body">
-                                <span className="card-number card-circle subtle">{currElem.id}</span>
-                                <span className="card-author subtle" style={myStyle}/* style={{color:"red"}} */>{currElem.category}</span>
-                                <h2 className="card-title">{currElem.name}</h2>
-                                <span className="card-description subtle">{currElem.description}</span>
+                                <span className="card-number card-circle subtle">{id}</span>
+                                <span className="card-author subtle" style={myStyle}/* style={{color:"red"}} */>{category}</span>
+                                <h2 className="card-title">{name}</h2>
+                                <span className="card-description subtle">{description}</span>
                                 <div className="card-read">Read</div>
                             </div>
-                            <img src={currElem.image} alt="images" className="card-media" />
+                            <img src={image} alt="images" className="card-media" />
                             <span className="card-tag subtle">Order Now</span>
                         </div>
                     </div>
--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,12 +1,34 @@
 import React,{useState} from 'react'
 import Menu from './MenuApi';
 import MenuCard from './MenuCard';
+import Navbar from './Navbar';
+
+const uniqueList=[
+    ...new Set(
+        Menu.map((currElem)=>{
+            return currElem.category;
+        })
+    ),
+    "All",
+];
+// console.log(uniqueList);
 
 const Restaurant = () => {
     const [menuData,setMenuData]=useState(Menu);
-
+    const [menuList,setMenuList]=useState(uniqueList);
+    const filterItem=(category)=>{
+        if(category==="All"){
+            setMenuData(Menu);
+            return;
+        }
+        const updatedList=Menu.filter((currElem)=>{
+            return currElem.category===category;
+        })
+        setMenuData(updatedList);
+    };
     return (
         <>
+            <Navbar filterItem={filterItem} menuList={menuList}/>
             <MenuCard menuData={menuData}/>
         </>
     )
