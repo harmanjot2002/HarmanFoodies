2695d33297aebc51d45b820dce26b06e352cb073
harmanjot2002 <harmanjot0555.be21@chitkara.edu.in>
2023-07-14 07:34:35 +0530

    Menu API

--- src/components/MenuApi.js
+++ src/components/MenuApi.js
@@ -8,7 +8,6 @@ const Menu = [
       description:
         "I love Maggi realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
     },
-  
     {
       id: 2,
       image: "images/allupakoida.jpg",
--- src/components/MenuCard.js
+++ src/components/MenuCard.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const MenuCard = ({menuData}) => {
-    console.log(menuData);
+    // console.log(menuData);
     const myStyle={color:"red"};
     return (
         <>
